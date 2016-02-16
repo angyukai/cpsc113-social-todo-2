db8e6dc3957d2529b620aff839479e0d4a4c3e82
angyukai <angyukai1@hotmail.com>
2016-02-16 20:44:43 +0000

    Added splash and trying out registration form stuff

--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,24 @@
 from __future__ import unicode_literals
-
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+#Do i put my users here?
+
 
 class Task(models.Model):
     owner = models.ForeignKey(User, related_name="owned_tasks")
     title = models.CharField(max_length=500)
     description = models.CharField(max_length=5000)
     collaborators = models.ManyToManyField(User, related_name="tasks")
+    def __str__(self):
+        # return self.owner
+        return self.title
+        return self.description
 
+# class TestQuestion(models.Model):
+#     q_text = models.CharField(max_length=500)
+#     q_name = models.CharField(max_length=500)
+#     def __str__(self):
+#         return self.q_text
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.template import loader
+from .models import Task
 
 # Create your views here.
 
+# def home(request):
+    # return HttpResponse("Hi you're at the home page")
+    
+# def my_view(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(username=username, password=password)
+#     if user is not None:
+#         if user.is_active:
+#             login(request, user)
+#             # Redirect to a success page.
+#         else:
+#             # Return a 'disabled account' error message
+#             ...
+#     else:
+#         # Return an 'invalid login' error message.
+#         ...
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+    task_list = Task.objects.all()
+    context = {'task_list': task_list}
+    template = loader.get_template("tasks/index.html")
+    return HttpResponse(template.render(context, request))
     
 def say_whatsup(request):
     return HttpResponse("Hello, WHAT IS UP?")
+    
+    
+def detail(request, task_id):
+    
+    return HttpResponse("You're looking at task %s." % task_id)
