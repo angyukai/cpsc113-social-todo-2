843833f55c58c672b99561d212c62e9ea245c5d8
angyukai <angyukai1@hotmail.com>
2016-02-17 00:49:08 +0000

    added registration forms successfully

--- splash/views.py
+++ splash/views.py
@@ -1,60 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import UserForm
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from forms import UserRegistrationForm
 # Create your views here.
 
 
 # Create your views here.
+
+# def index(request):
+#     return render(request, 'splash/index.html')
+    
 
 def index(request):
-    return render(request, 'splash/index.html')
     
-def register(request):
-    # Like before, get the request's context.
-    context = RequestContext(request)
-
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
+    
+    
+    
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(data=request.POST)
-
-        # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
-
-
-            # Update our variable to tell the template registration was successful.
-            registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/")
     else:
-        user_form = UserForm()
-
-    # Render the template depending on the context.
-    return render_to_response(
-            '/register.html',
-            {'user_form': user_form, 'registered': registered},
-            context)
-    
-    
+        form = UserRegistrationForm()
+    return render(request, "splash/index.html", {'form': form,})
